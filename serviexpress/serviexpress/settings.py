5e68ced468a46e125ee0cdf77b9ed32a15a5062b
Sal-Del-Este2 <este.salas@duocuc.cl>
2024-12-15 23:38:36 -0300

    cambio en restringir contraseñas, mas debiles para prueba

--- serviexpress/serviexpress/settings.py
+++ serviexpress/serviexpress/settings.py
@@ -76,6 +76,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
 #AUTH_PASSWORD_VALIDATORS = [
 #    { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',}, #contrasañe diferente al nombre de usuario
 #    { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',}, # contraseñas tengan una longitud mínima, defecto 8
